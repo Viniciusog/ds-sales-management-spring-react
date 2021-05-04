2a2b252888a3adbe9cb055c1c41856e0b4902af0
viniciusog <vinijipa23@gmail.com>
2021-05-04 14:44:39 -0400

    Project clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+/*Este é o nosso componente principal do REACT
+Temos a função App que retorna um HTML. Como elea retorna um HTML, podemos utilizar
+essa função como sendo um componente no REACT
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Olá mundo, Beleza?</h1>
     </div>
   );
 }
+
 
 export default App;
