8d215e6965151417a13fb7c72efd21828073876e
viniciusog <vinijipa23@gmail.com>
2021-05-04 16:35:53 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import DataTable from './components/DataTable'
+
 /*Este é o nosso componente principal do REACT
 Temos a função App que retorna um HTML. Como elea retorna um HTML, podemos utilizar
-essa função como sendo um componente no REACT
-*/
+essa função como sendo um componente no REACT*/
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo, Beleza?</h1>
-    </div>
+    /**Aqui dentro ficará todos os nossos componentes */
+    /* <> serve para que possamos retornar tags irmãs ao mesmo tempo*/
+    <> 
+      <NavBar />
+      <div>
+        <h1 className="text-primary">Olá mundo, Beleza?</h1>
+        <DataTable />
+      </div>
+      <Footer/>
+    </>
   );
 }
 
