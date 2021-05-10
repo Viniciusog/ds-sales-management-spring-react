cf4b229f8008e6f30fee2b0e928d7a33a6fa5099
viniciusog <vinijipa23@gmail.com>
2021-05-09 21:32:59 -0400

    DonutChart integration

--- frontend/src/components/DonutChart.tsx
+++ frontend/src/components/DonutChart.tsx
@@ -1,8 +1,9 @@
 import Chart from 'react-apexcharts'
 //AXIOS REALIZA EXECUTA MÉTODOS HTTPS (get post put, delete etc)
 import axios from 'axios'
-import {BASE_URL} from "../utils/requests"
+import { BASE_URL } from "../utils/requests"
 import { SaleSum } from 'types/sale'
+import { useEffect, useState } from 'react'
 
 
 //Dados do gráfico
@@ -11,28 +12,44 @@ type ChartData = {
     series: number[]
 }
 
-function DonutChart () {
-    //FORMA ERRADA
-    //Estamos instanciando uma variável de tipo ChartData e colocando os valores de labels e series como [], por padrão
-    let chartData: ChartData = {labels: [], series: []}
+function DonutChart() {
+    //chartData => Nome do nosso objeto
+    //setChartData => Nome da função que mudará o valor do nosso objeto chartData
+    //<ChartData> => Tipo padrão do nosso objeto
+    //({labels: [], series: []}) => Nome dos atributos e valores iniciais que nosso objeto terá
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
-    //FORMA ERRADA
-    //response.data() é o corpo da resposta
-    //Por padrão, as requisições feitas pelo AXIOS são ASSÍNCRONAS, ou seja:
-    //A nossa aplicação continuará funcionando normalmente até que o resultados seja carregado com sucesso pelo axios
-    //Por isso, temos que usar  'then' para executar uma função quando o axios carregar a response com sucesso
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            //Cast dos dados para o tipo SaleSum
-            const data = response.data as SaleSum[]
-            const myLabels = data.map(obj => obj.sellerName)
-            const mySeries = data.map(obj => obj.sum)
 
-            //Passando os valores do banco de dados para o nosso objeto chartData
-            chartData = {labels: myLabels, series: mySeries}
+    //Tem dois parâmetros, o primeiro é a função que irá executar e o segundo é o objeto que será sempre escutado
+    useEffect(() => {
+        //response.data() é o corpo da resposta
+        //Por padrão, as requisições feitas pelo AXIOS são ASSÍNCRONAS, ou seja:
+        //A nossa aplicação continuará funcionando normalmente até que o resultados seja carregado com sucesso pelo axios
+        //Por isso, temos que usar  'then' para executar uma função quando o axios carregar a response com sucesso
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                //Cast dos dados para o tipo SaleSum
+                const data = response.data as SaleSum[]
+                const myLabels = data.map(obj => obj.sellerName)
+                const mySeries = data.map(obj => obj.sum)
 
-            console.log(chartData)
-        })
+                //Passando os valores do banco de dados para o nosso objeto chartData
+                /*
+                APESAR DE CARREGAR AS RESPOSTAS DA REQUISIÇÃO E COLOCAR NA VARIÁVEL chartData,
+                O REACT CARREGA O GRÁFICO RAPIDAMENTE E NÃO ATUALIZA O GRÁFICO QUANDO A RESPONSE ESTÁ
+                PRONTA, por causa do assíncrono. 
+    
+                SENDO ASSIM, TEMOS QUE USAR useState PARA QUE O REACT POSSA IDENTIFICAR O ESTADO DO NOSSO OBJETO
+                E ASSIM ATUALIZAR O GRÁFICO QUANDO A RESPONSE CARREGAR
+    
+                useState: Um único objeto para todo o ciclo de vida do REACT. O objeto é constante mas seus dados podem
+                ser alterados*/
+                setChartData({ labels: myLabels, series: mySeries })
+
+                console.log(chartData)
+            })
+    }, [])
+
     /*Soma total de vendas por vendedor
     const mockData = {
         //Valor de cada parte do gráfico de Donut
@@ -40,7 +57,7 @@ function DonutChart () {
         //Legenda para cada um dos valores do gráfico de Donut
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }*/
-    
+
     const options = {
         legend: {
             show: true
@@ -48,11 +65,11 @@ function DonutChart () {
     }
 
     return (
-        <Chart options={{ ...options, labels: chartData.labels}}
-        series={chartData.series}
-        type="donut"
-        height="240"
-    />
+        <Chart options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
     );
 }
 
